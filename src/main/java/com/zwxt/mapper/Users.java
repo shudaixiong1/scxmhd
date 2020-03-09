d0dc71e1c990150f83bd107cc2b6055523c18b6d
lyc <17752535456@163.com>
2020-03-10 00:22:06 +0800

    mavenweb-git第一次提交到本地库

---
+++ src/main/java/com/zwxt/mapper/Users.java
@@ -0,0 +1,15 @@
+package com.zwxt.mapper;
+
+/**
+ * @description:
+ * @projectName:mavenweb-git
+ * @see:com.zwxt.mapper
+ * @author:liyongchao
+ * @createTime:2020/3/10 0:21
+ * @version:1.0
+ */
+public class Users {
+    public static void main(String[] args) {
+        System.out.println("456");
+    }
+}
