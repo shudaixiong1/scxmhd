14a04564a6bf943127bcde5269e74ac7fa903365
lyc <17752535456@163.com>
2020-03-09 22:07:51 +0800

    mavenweb-git第一次提交到本地库

---
+++ src/main/java/com/zwxt/mapper/UserMapper.java
@@ -0,0 +1,20 @@
+package com.zwxt.mapper;
+
+import com.zwxt.pojo.User;
+
+import java.util.List;
+
+/**
+ * @description:
+ * @projectName:mavenweb-git
+ * @see:com.zwxt.mapper
+ * @author:liyongchao
+ * @createTime:2020/3/9 22:05
+ * @version:1.0
+ */
+public interface UserMapper {
+    /**
+     * 查询用户信息
+     */
+    public List<User> getUsers();
+}
